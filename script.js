cc9ea0a31eca245843ee7794c1a58ff556dd0a0a
RajaRanjithkumar17 <rajaranjithkumar17.com>
2022-10-13 12:22:07 +0530

    my commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function add(a,b){
+    console.log(a+b);
+
+}add(4,5)
+
+function sub(a,b){
+
+    setTimeout(function(){
+
+        console.log(a-b);
+    },2000)
+
+}sub(4,5)
+
+function div( add, sub){
+    setTimeout(function(){
+
+        console.log("rr");
+
+        add( 2,2,sub(1,8))
+       
+    },4000)
+    
+
+}
+div(add,sub)
+
+
+
